317f8fd23cc593eea6a480d4ce4b44f438be45a0
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-09-01 22:45:16 +0530

    c1

---
+++ Fun-Extension/Content.js
@@ -0,0 +1,68 @@
+const generateSTYLES = () => {
+    return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
+    body {
+        background-image: url("https://i.pinimg.com/originals/f5/9c/e3/f59ce3c50ff404d3e0531e9306ef2864.jpg");
+        height: 500px; 
+        background-repeat: no-repeat;
+        background-position: center;
+    
+      font-family: "Open Sans", sans-serif;
+      max-height: 700px;
+      overflow: hidden;
+    }
+   
+     </style>`;
+  };
+  
+  const generateHTML = (pageName) => {
+    return `
+     
+     <div id="clouds">
+        <div class="cloud x1"></div>
+        <div class="cloud x1_5"></div>
+        <div class="cloud x2"></div>
+        <div class="cloud x3"></div>
+        <div class="cloud x4"></div>
+        <div class="cloud x5"></div>
+    </div>
+    <div class='c'>
+        
+        <div class='_1'>This Device Is Not You</div>
+        <div class='_2'>Please Leave There > ${pageName}</div>
+    </div>
+     `;
+  };
+
+  
+  switch (window.location.hostname) {
+    case "www.youtube.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("YOUTUBE");
+      break;
+    case "www.facebook.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("FACEBOOK");
+      break;
+    case "www.netflix.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("NETFLIX");
+      break;
+    case "www.roblox.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("ROBLOX");
+      break;
+    case "discord.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("DISCORD");
+      break;
+    case "www.spotify.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("SPOTIFY");
+      break;
+      case "www.whatsapp.com":
+        document.head.innerHTML = generateSTYLES();
+        document.body.innerHTML = generateHTML("WHATSAPP");
+        break;
+        
+  }
+  
